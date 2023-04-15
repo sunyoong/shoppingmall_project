f84b1965e88a5eca9077e91dac498e7ff585188c
박선영 <haert2010@naver.com>
2023-04-15 19:30:39 +0900

    P 01. - Springboot 쇼핑몰 프로젝트 - entity, constant 패키지 생성 - yaml 파일 설정

---
+++ product/src/main/java/com/shop/product/constant/ItemSellStatus.java
@@ -0,0 +1,6 @@
+package com.shop.product.constant;
+
+// 상품이 현재 판매중인지, 품절 상태인지 나타내는 클래스
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ product/src/main/java/com/shop/product/entity/Item.java
@@ -0,0 +1,13 @@
+package com.shop.product.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Item {
+
+}
